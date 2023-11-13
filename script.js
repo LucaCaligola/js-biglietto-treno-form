78625cd0527e2dfaaa2c81fd9e142f30a3112fd3
LucaCaligola <luca.caligola@gmail.com>
2023-11-13 17:25:56 +0100

    working on the css of the ticket

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const invia = document.querySelector ('button');
 
 invia.addEventListener ('click',function () {
+
     const km = parseFloat(document.querySelector("#km").value);
     const anni = parseInt(document.querySelector("#anni").value);
+    const credenziali = document.querySelector("#credenziali").value;
+
     
-
-
     let prezzoPerKm = 0.1927
 
 
@@ -19,7 +20,7 @@ invia.addEventListener ('click',function () {
 
     let prezzoTicket = (prezzoPerKm * km).toFixed(2);
         console.log (prezzoTicket);
-        
-}
 
-)
+})
+
+// document.getElementById ("output").innerHTML = 'il costo del biglietto è di: ' + prezzoTicket
