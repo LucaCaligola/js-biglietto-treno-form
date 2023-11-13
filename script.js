82904e5e26e60471bca8e134c9d07192becceee8
LucaCaligola <luca.caligola@gmail.com>
2023-11-13 16:53:31 +0100

    Milestone 1 done

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-const km = parseFloat(document.querySelector("#km"));
-const anni = parseInt(document.querySelector("#anni"));
 const invia = document.querySelector ('button');
 
-
-
-
-let prezzoPerKm = 0.1927
-
-
-if (anni < 18) {
-    prezzoPerKm=prezzoPerKm * 0.8235
-}
-
-else if (anni > 64) {
-    prezzoPerKm=prezzoPerKm * 0.4673
-}
-
-let prezzoTicket = (prezzoPerKm * km).toFixed(2);
-
 invia.addEventListener ('click',function () {
-    console.log (prezzoTicket);
+    const km = parseFloat(document.querySelector("#km").value);
+    const anni = parseInt(document.querySelector("#anni").value);
     
+
+
+    let prezzoPerKm = 0.1927
+
+
+    if (anni < 18) {
+        prezzoPerKm=prezzoPerKm * 0.8235
+    }
+
+    else if (anni > 64) {
+        prezzoPerKm=prezzoPerKm * 0.4673
+    }
+
+    let prezzoTicket = (prezzoPerKm * km).toFixed(2);
+        console.log (prezzoTicket);
+        
 }
 
 )
